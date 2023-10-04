69271ab562300f45135616ccb28f6a0d64b4d474
Habtamu <habtamugirum0489@gmail.com>
2023-10-04 22:36:03 +0300

    condtion

---
+++ conditon.py
@@ -0,0 +1,18 @@
+name = "habtamu"
+age = 24
+
+if name=="habtamu" and age==24:
+    print("your name is habtamu and your age is 24")
+
+else:
+    print("your name is not habtamu or your age is not 24")
+
+
+# in operator
+
+name = "habtamu"
+namelist=["habtamu", "haile"]
+if name in namelist:
+    print("your name either habtamu or haile")
+else:
+    print("your name is not habtamu and haile")
